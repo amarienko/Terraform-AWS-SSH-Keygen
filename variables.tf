3263dd97a33effd6a4e1892464c86339629262ea
Alex Marienko <alexey.maryenko@icloud.com>
2023-01-11 04:23:27 +0200

    Variables update.

--- variables.tf
+++ variables.tf
@@ -1,12 +1,4 @@
 # Variables declaration
-/*
-variable "region" {
-  description = "AWS Region"
-  type        = string
-  default     = "eu-west-1"
-}
-*/
-
 variable "all_tags" {
   description = "Global tags for each resource"
   type        = map(string)
